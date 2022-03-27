a0fc072108b3ab9953e69ed61e67a31c268c1253
raineepei <pei.rainee@gmail.com>
2022-03-27 14:12:19 -0700

    fix

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -80,7 +80,7 @@ export const about = [
     description1: "rainee is currently a student at uc san diego double majoring in bioinformatics and cognitive science: design and interaction.",
     description2: "she's passionate about the intersection of tech and design and is always looking for opportunities to engage in both creative and technical work.",
     description3: "in her free time, she loves to shoot film, climb, and skateboard. she also loves her slightly chunky cat, dally.",
-    tags: ['React', 'JavaScript'],
+    tags: ['React', 'JavaScript'], 
     source: 'https://google.com',
     visit: 'https://google.com',
     id: 1,
