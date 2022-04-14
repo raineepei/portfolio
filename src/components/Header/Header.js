89deb469d87d90c55c3c20b7e2512e6811ccb322
raineepei <pei.rainee@gmail.com>
2022-04-14 13:00:13 -0700

    update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,7 +11,7 @@ const Header = () =>  (
     <Div1>
       <Link href="/">
         <a style={{ display:"flex", alignItems: "center", color: "black", fontWeight: "bold" }}>
-          <Span>rainee pei.</Span>
+          <Span>rainee pei</Span>
         </a>
       </Link>
     </Div1>
